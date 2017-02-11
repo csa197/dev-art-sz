0a55f91fab51894022404295b6b67232ef2ff518
csa197 <albert.kirchmeyr@gmail.com>
2017-02-11 17:37:23 +0100

    re

--- _init.php
+++ _init.php
@@ -19,7 +19,7 @@
 // Here we also assign default values for each of them.
 $title = $page->get('headline|title'); 
 $content = $page->body;
-$sidebar = $page->sidebar2;
+$sidebar = $page->sidebar;
 
 // We refer to our homepage a few times in our site, so 
 // we preload a copy here in $homepage for convenience. 
